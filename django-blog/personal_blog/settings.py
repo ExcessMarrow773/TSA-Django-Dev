a782294a09d1e7cec2a86db164de4a0c6ed4466b
ExcessMarrow773 <atticus.falkner.walton@gmail.com>
2025-02-26 19:26:33 -0600

    adds secret_key things

--- django-blog/personal_blog/settings.py
+++ django-blog/personal_blog/settings.py
@@ -18,7 +18,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-SECRET_KEY = os.environ['SECRET_KEY']
+if os.name == "win32":
+    with open("key.txt", "r") as key_file:
+        SECRET_KEY = key_file.read().strip()
+else:
+    SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
